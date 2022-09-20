ffadf27ed6393286780911f205822409f34335e7
João Marques <jmarquesm@users.noreply.github.com>
2022-09-20 18:20:32 -0400

    Implementing contact page

---
+++ src/pages/contato.jsx
@@ -0,0 +1,133 @@
+import {
+  createStyles,
+  ThemeIcon,
+  Text,
+  SimpleGrid,
+  Box,
+  Stack,
+  Container,
+  Title,
+} from "@mantine/core";
+import { IconSun, IconPhone, IconMapPin, IconAt } from "@tabler/icons";
+import Layout from "../components/common/Layout";
+
+const useStyles = createStyles((theme, { variant }) => ({
+  wrapper: {
+    display: "flex",
+    alignItems: "center",
+    color: theme.white,
+  },
+
+  icon: {
+    marginRight: theme.spacing.md,
+    backgroundImage:
+      variant === "gradient"
+        ? `linear-gradient(135deg, ${theme.colors[theme.primaryColor][4]} 0%, ${
+            theme.colors[theme.primaryColor][6]
+          } 100%)`
+        : "none",
+    backgroundColor: "transparent",
+  },
+
+  title: {
+    color:
+      variant === "gradient"
+        ? theme.colors.gray[6]
+        : theme.colors[theme.primaryColor][0],
+  },
+
+  description: {
+    color: variant === "gradient" ? theme.black : theme.white,
+  },
+}));
+
+function ContactIcon({
+  icon: Icon,
+  title,
+  description,
+  variant = "gradient",
+  className,
+  ...others
+}) {
+  const { classes, cx } = useStyles({ variant });
+  return (
+    <div className={cx(classes.wrapper, className)} {...others}>
+      {variant === "gradient" ? (
+        <ThemeIcon size={40} radius="md" className={classes.icon}>
+          <Icon size={24} />
+        </ThemeIcon>
+      ) : (
+        <Box mr="md">
+          <Icon size={24} />
+        </Box>
+      )}
+
+      <div>
+        <Text size="xs" className={classes.title}>
+          {title}
+        </Text>
+        <Text className={classes.description}>{description}</Text>
+      </div>
+    </div>
+  );
+}
+
+const MOCKDATA = [
+  { title: "Email", description: "jmarquesm@live.com", icon: IconAt },
+  { title: "Telefone", description: "+55 (066) 99649 - ****", icon: IconPhone },
+  {
+    title: "Endereço",
+    description: "Rondonópolis - Mato Grosso",
+    icon: IconMapPin,
+  },
+  {
+    title: "Horario de atendimento",
+    description: "8:00 – 11:00 | 13:00 - 17:00.",
+    icon: IconSun,
+  },
+];
+
+function ContactIconsList({ data = MOCKDATA, variant }) {
+  const items = data.map((item, index) => (
+    <ContactIcon key={index} variant={variant} {...item} />
+  ));
+  return <Stack>{items}</Stack>;
+}
+
+export default function ContactIcons() {
+  return (
+    <Layout>
+      <Container
+        style={{
+          minHeight: "calc(100vh - 140px)",
+        }}
+      >
+        <Title size={"h2"} my={"md"}>
+          Contato
+        </Title>
+        <SimpleGrid cols={2} breakpoints={[{ maxWidth: 755, cols: 1 }]}>
+          <Box
+            sx={(theme) => ({
+              padding: theme.spacing.xl,
+              borderRadius: theme.radius.md,
+              backgroundColor: theme.white,
+            })}
+          >
+            <ContactIconsList />
+          </Box>
+          <Box
+            sx={(theme) => ({
+              padding: theme.spacing.xl,
+              borderRadius: theme.radius.md,
+              backgroundImage: `linear-gradient(135deg, ${
+                theme.colors[theme.primaryColor][6]
+              } 0%, ${theme.colors[theme.primaryColor][4]} 100%)`,
+            })}
+          >
+            <ContactIconsList variant="white" />
+          </Box>
+        </SimpleGrid>
+      </Container>
+    </Layout>
+  );
+}
