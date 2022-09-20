1d8649d126fad05574c243f24dc30e85ba25a5ee
João Marques <jmarquesm@users.noreply.github.com>
2022-09-20 11:13:01 -0400

    Adjusting the loader

--- src/pages/api/estatistica.js
+++ src/pages/api/estatistica.js
@@ -1,9 +1,4 @@
 export default async function handler(req, res) {
-  await new Promise(resolve => {
-    setTimeout(() => {
-      resolve()
-    }, 2000);
-  })
   res.status(200).json([
     {
       title: "Visitantes",
