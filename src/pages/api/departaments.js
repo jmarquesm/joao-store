c1f6f414b86db4be1e5a20090e561b092c5ffce1
Joao Marques <jmarquesm@users.noreply.github.com>
2022-10-05 13:35:07 -0400

    fix apis

--- src/pages/api/departaments.js
+++ src/pages/api/departaments.js
@@ -3,8 +3,6 @@ import categories from "../../data/categories"
 export default function handler(req, res) {
 
     res.status(200).json({
-        id: "7",
         items: categories,
-        name: "Departamentos",
     })
 }
