e41bddc8c34d402f08dc4122ed5b659f58eb7c31
Joao Marques <jmarquesm@users.noreply.github.com>
2023-02-14 11:19:24 -0400

    refactor: remove css from index component and improve imports

---
+++ src/components/common/Header/Header.jsx
@@ -0,0 +1,76 @@
+// vendors
+import { Group, Burger, Popover, ActionIcon, Indicator } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import Link from "next/link";
+
+// components
+import { NavBar } from "../../navBar/NavBar";
+import { DarkThemeButton } from "../DarkThemeButton";
+import { MenuBox } from "../MenuBox";
+import { UseModal } from "../../account/UseModal/UseModal";
+import { SearchBar } from "../../SearchBar";
+
+// icons
+import { IconShoppingCart } from "@tabler/icons";
+
+// styles
+import * as S from "./styles";
+
+const pagesMock = [
+  { label: "Pagina Inicial", link: "/" },
+  { label: "Produtos", component: MenuBox },
+  { label: "Conta", component: UseModal },
+];
+
+export function Header({ items }) {
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const itemsEL = pagesMock.map((item) => {
+    if (item.component) {
+      return <item.component key={item.label} />;
+    }
+
+    return (
+      <Link key={item.label} href={item.link}>
+        <S.Link>{item.label}</S.Link>
+      </Link>
+    );
+  });
+
+  const totalDeItensDoCarrinho = items.reduce((acc, product) => acc + product.units, 0);
+
+  return (
+    <S.Header height={56} mb={0}>
+      <S.Nav>
+        <Group>
+          <Popover opened={opened} closeOnClickOutside onClick={toggle} size="sm">
+            <Popover.Target>
+              <Burger opened={opened} onClick={toggle} size="sm" />
+            </Popover.Target>
+            <Popover.Dropdown p={0} ml={8}>
+              <NavBar />
+            </Popover.Dropdown>
+          </Popover>
+        </Group>
+
+        <Group>
+          <S.GroupLinks ml={50} spacing={5}>
+            {itemsEL}
+          </S.GroupLinks>
+
+          <SearchBar />
+
+          <Indicator label={totalDeItensDoCarrinho} inline size={15} overflowCount={99}>
+            <Link href={"/carrinho"}>
+              <ActionIcon color="blue">
+                <IconShoppingCart size={22} />
+              </ActionIcon>
+            </Link>
+          </Indicator>
+
+          <DarkThemeButton />
+        </Group>
+      </S.Nav>
+    </S.Header>
+  );
+}
