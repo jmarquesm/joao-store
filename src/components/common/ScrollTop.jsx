61d3ca40f8d8c71577aa83d1e6f8eaf6a3eec4dd
Joao Marques <jmarquesm@users.noreply.github.com>
2023-02-14 11:27:22 -0400

    refactor: improve imports

--- src/components/common/ScrollTop.jsx
+++ src/components/common/ScrollTop.jsx
@@ -1,6 +1,9 @@
-import { IconArrowUp } from "@tabler/icons";
+// vendors
 import { useWindowScroll } from "@mantine/hooks";
 import { Affix, Button, Transition } from "@mantine/core";
+
+// icons
+import { IconArrowUp } from "@tabler/icons";
 
 export function ScrollTop() {
   const [scroll, scrollTo] = useWindowScroll();
@@ -10,11 +13,7 @@ export function ScrollTop() {
       <Affix position={{ bottom: 20, right: 20 }}>
         <Transition transition="slide-up" mounted={scroll.y > 0}>
           {(transitionStyles) => (
-            <Button
-              p={8}
-              style={transitionStyles}
-              onClick={() => scrollTo({ y: 0 })}
-            >
+            <Button p={8} style={transitionStyles} onClick={() => scrollTo({ y: 0 })}>
               <IconArrowUp size={16} />
             </Button>
           )}
