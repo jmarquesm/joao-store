ec3f1812228093f0186453b88e17bba4acba46c5
Joao Marques <jmarquesm@users.noreply.github.com>
2023-02-15 13:46:43 -0400

    refactor: remove css from index component and improve imports

--- src/pages/carrinho.jsx
+++ src/pages/carrinho.jsx
@@ -1,39 +1,20 @@
-import styled from "@emotion/styled";
-import { Container, Text } from "@mantine/core";
-import Layout from "../components/common/Layout";
+// components
+import Layout from "../components/common/Layout/Layout";
 import { Cart } from "../components/purchase/Cart";
 
-const StyledContainerTitle = styled(Container)`
-  min-height: calc(100vh - 130px);
-  padding: 16px;
-  padding-top: 0px;
-`;
-
-const StyledContainerCartEmpty = styled(Container)`
-  display: flex;
-  justify-content: center;
-`;
-
-const StyledTextCartEmpty = styled(Text)`
-  font-size: 50px;
-  font-weight: 600;
-  color: #49505757;
-`;
+// styles
+import * as S from "../styles/carrinho";
 
 export default function Carrinho({ items, setItems }) {
   return (
     <Layout items={items} setItems={setItems}>
-      <StyledContainerTitle>
-        {items === null || items.length === 0 || items === [] ? (
-          <StyledContainerCartEmpty my={200}>
-            <StyledTextCartEmpty>Carrinho Vazio</StyledTextCartEmpty>
-          </StyledContainerCartEmpty>
-        ) : (
-          <>
-            <Cart items={items} setItems={setItems} />
-          </>
-        )}
-      </StyledContainerTitle>
+      {items === null || items.length === 0 || items === [] ? (
+        <S.CartIsEmpty my={200}>
+          <S.TextCartIsEmpty>Carrinho Vazio</S.TextCartIsEmpty>
+        </S.CartIsEmpty>
+      ) : (
+        <Cart items={items} setItems={setItems} />
+      )}
     </Layout>
   );
 }
---
+++ src/styles/carrinho.js
@@ -0,0 +1,14 @@
+// vendors
+import styled from "@emotion/styled";
+import { Container, Text } from "@mantine/core";
+
+export const CartIsEmpty = styled(Container)`
+  display: flex;
+  justify-content: center;
+`;
+
+export const TextCartIsEmpty = styled(Text)`
+  font-size: 50px;
+  font-weight: 600;
+  color: #49505757;
+`;
