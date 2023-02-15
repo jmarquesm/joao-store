1144e5d9159c53759f3b69acde2215f6a824efde
Joao Marques <jmarquesm@users.noreply.github.com>
2023-02-15 13:16:02 -0400

    styles: improve imports

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,7 @@
+// vendors
 import { createGetInitialProps } from "@mantine/next";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+
 const getInitialProps = createGetInitialProps();
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
