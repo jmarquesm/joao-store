0b48848c0a3442afd920466083f77602478ca160
Joao Marques <jmarquesm@users.noreply.github.com>
2023-02-15 14:28:59 -0400

    fix: set a height for userbutton

--- src/components/navBar/userButton/styles.js
+++ src/components/navBar/userButton/styles.js
@@ -7,6 +7,7 @@ export const ButtonUser = styled(UnstyledButton)`
   width: 100%;
   padding: ${({ theme }) => theme.spacing.md};
   color: ${({ theme }) => (theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black)};
+  height: 60px;
 
   &:hover {
     background-color: ${({ theme }) =>
